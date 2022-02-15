799bf7c0f7bc1ef0d5f7d79ade63e66eb74c9241
ShitalBhise <bhiseshital27059@gmail.com>
2022-02-15 18:57:08 +0530

    Added: Welcome Message

--- src/com/java/LineCompareCmp.java
+++ src/com/java/LineCompareCmp.java
@@ -3,27 +3,7 @@ package com.java;
 public class LineCompareCmp {
 	  public static void main(String[] args) {
 
-	Scanner scanner = new Scanner(System.in);
-    
-    System.out.println("Please enter x1");
-    int x1 = scanner.nextInt();
-   
-    System.out.println("Please enter y1");
-    int y1 = scanner.nextInt();
-   
-    System.out.println("Please enter x2");
-    int x2 = scanner.nextInt();
-   
-    System.out.println("Please enter y2");
-    int y2 = scanner.nextInt();
-   
-    int val1 = (int) Math.pow((x2-x1), 2);
-    int val2 = (int) Math.pow((y2-y1), 2);
-    int result = (int) Math.sqrt(val1+val2);
-   
-    System.out.println("Langth of line = "+result);
-   
-    scanner.close();
+		  System.out.println("Welcome to Line Comparing Computation");
    }
 } 
 	
@@ -31,4 +11,4 @@ public class LineCompareCmp {
 	
 	
 	
-}
+
