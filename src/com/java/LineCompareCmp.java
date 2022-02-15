091412a6231364706792fea453b113a64682c2af
ShitalBhise <bhiseshital27059@gmail.com>
2022-02-15 20:24:51 +0530

    Added: Line Check-Equal/grater than/Less than logic

--- src/com/java/LineCompareCmp.java
+++ src/com/java/LineCompareCmp.java
@@ -3,41 +3,37 @@ package com.java;
 import java.util.Scanner;
 
 public class LineCompareCmp {
-	public static int getLengthOfLine(Scanner scanner) {
-		System.out.println("Please enter x1");
-		int x1 = scanner.nextInt();
+	public static int lineLengthCal() {
+		int x1, y1, x2, y2;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter lines : x1,x2,x3,x4");
+		x1 = scanner.nextInt();
+		y1 = scanner.nextInt();
+		x2 = scanner.nextInt();
+		y2 = scanner.nextInt();
+		int lineLength = (int) Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+		return lineLength;
 
-		System.out.println("Please enter y1");
-		int y1 = scanner.nextInt();
+	}
 
-		System.out.println("Please enter x2");
-		int x2 = scanner.nextInt();
-
-		System.out.println("Please enter y2");
-		int y2 = scanner.nextInt();
-
-		int val1 = (int) Math.pow((x2 - x1), 2);
-		int val2 = (int) Math.pow((y2 - y1), 2);
-		int result = (int) Math.sqrt(val1 + val2);
-
-		return result;
+	public static void equalityCheck(int lineOneLength, int lineTwoLength) {
+		if (lineOneLength == lineTwoLength) {
+			System.out.println("Lines are Equal");
+		} else if (lineOneLength > lineTwoLength) {
+			System.out.println("Line1 is greater than Line2 ");
+		} else {
+			System.out.println("Line2 is greater than Line1 ");
+		}
 	}
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please enter values for line 1");
-		int lenthOfline_1 = getLengthOfLine(scanner);
-		System.out.println("Please enter values for line 2");
-		int lenthOfline_2 = getLengthOfLine(scanner);
+		System.out.println("Welcome to Line_check" + "Comparison Computation" + "Program on Master Branch");
+		int lineOneLength = lineLengthCal();
+		int lineTwoLength = lineLengthCal();
+		System.out.println("The length of line 1 is: " + lineOneLength);
+		System.out.println("The length of line 2 is: " + lineTwoLength);
+		equalityCheck(lineOneLength, lineTwoLength);
 
-		String val1 = String.valueOf(lenthOfline_1);
-		String val2 = String.valueOf(lenthOfline_2);
-
-		if (val1.equals(val2)) {
-			System.out.println("Both lines are equal");
-		} else {
-			System.out.println("Both lines are not equal");
-		}
-		scanner.close();
 	}
+
 }
